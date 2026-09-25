c3ec65afe9484ad405cf9c90b4fa7a403d42f45f
João Pedro Ribeiro da Silva <joaopedrorib@hotmail.com>
2026-09-25 14:01:43 -0300

    fix(M00_0)

--- M00/0.cpp
+++ M00/0.cpp
@@ -5,21 +5,20 @@ using namespace std;
 int m[1000];
 
 struct Compare{
-    bool operator()(pair<float, int> a, pair<float, int> b){
-        if (a.first != b.first){
-            return a.first < b.first;
-        }
-
-        return a.second > b.second;
+    bool operator()(pair<int, int> a, pair<int, int> b){
+        return a.first / a.second < b.first / b.second;
     }
 };
 
 
 int main(){
-    int p, n;
-    while(cin >> p >> n){
-        priority_queue<pair<float, int>, vector<pair<float, int>>, Compare> pq;
-        float m;
+    int N;
+    cin >> N;
+    for(int j = 0; j < N; j++){
+        int p, n;
+        cin >> p >> n;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+        int m;
         for(int i = 0; i < n; i++){
             cin >> m;
             pq.push({m, 1});
@@ -27,13 +26,14 @@ int main(){
 
         p -= n;
         for(int i=0; i<p; i++){
-            float a; int b;
+            int a; int b;
             tie(a, b) = pq.top(); pq.pop();
-            a = (a*b)/++b;
-            pq.push({a, b});
+            pq.push({a, b+1});
         }
 
-        cout << ceil(pq.top().first) << endl;
+        int a; int b;
+        tie(a, b) = pq.top();
+        cout << ceil(a/b) << endl;
     }
 
     return 0;
